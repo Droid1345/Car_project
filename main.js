86b422418ef4b5004f5430e066e9599644596ef5
Droid1345 <85997217+Droid1345@users.noreply.github.com>
2021-08-19 16:59:53 +0530

    Update main.js

--- main.js
+++ main.js
@@ -28,7 +28,71 @@ function uploadRover(){
 
 window.addEventListener("keydown",Mykeydown);
 
-function Mykeydown(e){
+window.addEventListener("keydown",mykeydown);
+function mykeydown(e){
     keypress=e.keyCode;
-    console.log("The Key pressed is :"+keypress);
+    }
+    if(keypress == '38'){
+        up();
+        console.log("up key pressed");
+    }
+    if(keypress == '40'){
+        down();
+        console.log("down key pressed");
+    }
+    if(keypress == '37'){
+        left();
+        console.log("left key pressed");
+    }
+    if(keypress == '39'){
+       right();
+        console.log("right key pressed");
+   
+}
+function up()
+{
+	if(player_y >=0)
+	{
+		player_y = player_y - block_image_height;
+		console.log("block image height = " + block_image_height);
+		console.log("When Up arrow key is pressed, X =  " + player_x + " , Y = "+player_y);
+		canvas.remove(player_object);
+		player_update();
+	}
+}
+
+function down()
+{
+	if(player_y <=500)
+	{
+		player_y = player_y + block_image_height;
+		console.log("block image height = " + block_image_height);
+		console.log("When Down arrow key is pressed, X =  " + player_x + " , Y = "+player_y);
+		canvas.remove(player_object);
+		player_update();
+	}
+}
+
+function left()
+{
+	if(player_x >0)
+	{
+		player_x = player_x - block_image_width;
+		console.log("block image width = " + block_image_width);
+		console.log("When Left arrow key is pressed, X =  " + player_x + " , Y = "+player_y);
+		canvas.remove(player_object);
+		player_update();
+	}
+}
+
+function right()
+{
+	if(player_x <=850)
+	{
+		player_x = player_x + block_image_width;
+		console.log("block image width = " + block_image_width);
+		console.log("When Right arrow key is pressed, X =  " + player_x + " , Y = "+player_y);
+		canvas.remove(player_object);
+		player_update();
+	}
 }
